f61f0eabe3bd560af2cd2dede2c9ba13ec2b931f
mCvilkas <tomas.godnia@gmail.com>
2016-12-14 20:43:13 +0200

    ++

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 var ekranas = document.getElementById('ekranas');
+var pirmasDaug;
 var skaicius = function(skaitmuo) {
   ekranas.value += skaitmuo;
   console.log(ekranas.value);
 };
 //console.console.log(this);
 var daugyba = function() {
-var pirmasDaug = ekranas.value;
+pirmasDaug = ekranas.value;
 ekranas.value = null;
 console.log(pirmasDaug);
+};
+
+var rezultatas = function(){
+
 };
